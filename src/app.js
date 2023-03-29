154eb60c86d554abbc5c516614d79b5041c46252
RobertoMK <roberto.kiefer@botcity.com.br>
2023-03-29 05:32:52 -0300

    ci fixes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const axios = require('axios')
 const cors = require('cors')
+const serverless = require('serverless-http')
 
 app.use(cors())
 app.use(express.json())
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     console.log(req.body)
     var data = {};
     axios.post('https://clft4x7md001ul508uw3ggiys.databerry.ai/query', req.body, {
@@ -19,9 +21,13 @@ app.post('/', (req, res) => {
             }).catch(function (error) {
             });
 })
-app.get('/cors', (req, res) => {
+router.get('/cors', (req, res) => {
     res.send('This has CORS enabled 🎈')
 })
+
+app.use('/.netlify/functions/api', router);
 app.listen(8080, () => {
     console.log('listening on port 8080')
 })
+
+module.exports.handler = serverless(app);
